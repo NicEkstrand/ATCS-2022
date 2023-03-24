aec305b42a95e2a999dcd6afea7dca5bd5116929
nicolasekstrand <nicolas.ekstrand@menloschool.org>
2023-03-24 09:54:38 -0700

    Start of Twitter

---
+++ twitter/models.py
@@ -0,0 +1,57 @@
+"""
+The file that holds the schema/classes
+that will be used to create objects
+and connect to data tables.
+"""
+
+from sqlalchemy import ForeignKey, Column, INTEGER, TEXT, DATETIME
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    # Columns
+    username = Column("username", TEXT, primary_key=True)
+    password = Column("password", TEXT, nullable=False)
+
+    following = relationship("User", 
+                             secondary="followers",
+                             primaryjoin="User.username==Follower.follower_id",
+                             secondaryjoin="User.username==Follower.following_id")
+    
+    followers = relationship("User", 
+                             secondary="followers",
+                             primaryjoin="User.username==Follower.following_id",
+                             secondaryjoin="User.username==Follower.follower_id",
+                             overlaps="following")
+    tweets = relationship("Tweet", back_populates="user")
+
+
+class Follower(Base):
+    __tablename__ = "followers"
+
+    # Columns
+    id = Column("id", INTEGER, primary_key=True)
+    follower_id = Column('follower_id', TEXT, ForeignKey('users.username'))
+    following_id = Column('following_id', TEXT, ForeignKey('users.username'))
+    
+
+class Tweet(Base):
+    __tablename__ = "tweets"
+    id = Column("id", INTEGER, primary_key=True)
+    content = Column("content", INTEGER)
+    timestamp = Column("timestamp", INTEGER)
+    username = Column("username", TEXT, ForeignKey("users.username"))
+    user = relationship("User", back_populates="tweets")
+
+class Tag(Base):
+    __tablename__ = "tags"
+    id = Column("id", INTEGER, primary_key=True)
+    content = Column("content", TEXT)
+
+class TweetTag(Base):
+    __tablename__ = "tweettags"
+    id = Column("id", INTEGER, primary_key=True)
+    tweet_id = Column("tweet_id", INTEGER, ForeignKey("tweets.id"))
+    tag_id = Column("tag_id", INTEGER, ForeignKey("tags.id"))
